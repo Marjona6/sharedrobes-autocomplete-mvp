5bdf42e0b15740def36eda3e1464a4c0e9bf8b7d
Meryem Yanar <meryem.yanaryanar@gmail.com>
2021-01-25 01:09:21 +0300

    Add header and tests for header

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders header', () => {
+  render(<App />)
+  const headerElement = screen.getByRole('header')
+  expect(headerElement).toBeInTheDocument()
+})
+
+test('renders text inside header', () => {
+  render(<App />)
+  const headerElementText = document.querySelector('h1')
+  expect(headerElementText).toBeInTheDocument()
+})
+
+// test("renders autocomplete input field", () => {
+//   render(<App />);
+//   const autocompleteInputElement = screen.findByRole("header"); //
+//   expect(autocompleteInputElement).toBeInTheDocument();
+// });
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header aria-label='header' name='header' role='header'>
+      <h1>Sharedrobes Smart Autocomplete Input Field MVP</h1>
+    </header>
+  )
+}
+
+export default Header
